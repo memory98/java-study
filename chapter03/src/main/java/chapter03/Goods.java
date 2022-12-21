9ad9d307c19d0949e0f5f686a87ad37f87191249
승학산-11 <-11@DESKTOP-LL8VBES>
2022-12-21 11:28:36 +0900

    정보은닉 class

--- chapter03/src/main/java/chapter03/Goods.java
+++ chapter03/src/main/java/chapter03/Goods.java
@@ -1,36 +1,56 @@
 package chapter03;
 
 public class Goods {
+	public static int countOfGoods = 0;
+
 	private String name;
 	private int price;
 	private int countStock;
 	private int countSold;
-	
-	public void printInfo() {
-		System.out.println(name + " : " + price + " : "+ countStock+" : "+countSold);
+
+	public Goods() {
+		countOfGoods = countOfGoods + 1;
+	}
+
+	public int calcDiscountPrice(float discountRate) {
+		return (int) (price*discountRate);
 	}
 	
+	public void printInfo() {
+		System.out.println(name + " : " + price + " : " + countStock + " : " + countSold);
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getPrice() {
 		return price;
 	}
+
 	public void setPrice(int price) {
+		if (price < 0) {
+			price = 0;
+		}
 		this.price = price;
 	}
+
 	public int getCountStock() {
 		return countStock;
 	}
+
 	public void setCountStock(int countStock) {
 		this.countStock = countStock;
 	}
+
 	public int getCountSold() {
 		return countSold;
 	}
+
 	public void setCountSold(int countSold) {
 		this.countSold = countSold;
 	}
