d3062d0f733dda873027c9c6780836ce2c350b35
memory98 <asdlkjh252567@gmail.com>
2022-12-29 15:31:41 +0900

    http

--- network/src/main/java/echo/EchoServer.java
+++ network/src/main/java/echo/EchoServer.java
@@ -1,14 +1,9 @@
 package echo;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.io.PrintWriter;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.SocketException;
 
 public class EchoServer {
 	public static final int PORT = 8000;
@@ -21,42 +16,44 @@ public class EchoServer {
 			serverSocket.bind(new InetSocketAddress("0.0.0.0", 8000), 10);
 			log("start....[port : " + PORT + "]");
 
-			Socket socket = serverSocket.accept();
-
-			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
-			String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
-			int remotePort = inetRemoteSocketAddress.getPort();
-			log("connected by client[" + remoteHostAddress + ":" + remotePort + "]");
-
-			try {
-				// 4. IO Stream 받아오기
-				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true); // true
-																													// 바로
-																													// 출력.
-				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
-
-				while (true) {
-					String data = br.readLine(); // 갱을 붙여라..?
-					if (data == null) {
-						log("closed by client");
-						break;
-					}
-					log("received : " + data);
-					pw.println(data);
-				}
-			} catch (SocketException ex) {
-				log("suddenly closed by client");
-			} catch (IOException ex) {
-				log("error : " + ex);
-			} finally {
-				try {
-					if (socket != null && !socket.isClosed()) {
-						socket.close();
-					}
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}
+			while(true) {
+				Socket socket = serverSocket.accept();
+				new EchoRequestHandler(socket).start();
 			}
+//			InetSocketAddress inetRemoteSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+//			String remoteHostAddress = inetRemoteSocketAddress.getAddress().getHostAddress();
+//			int remotePort = inetRemoteSocketAddress.getPort();
+//			log("connected by client[" + remoteHostAddress + ":" + remotePort + "]");
+//
+//			try {
+//				// 4. IO Stream 받아오기
+//				PrintWriter pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true); // true
+//																													// 바로
+//																													// 출력.
+//				BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+//
+//				while (true) {
+//					String data = br.readLine(); // 갱을 붙여라..?
+//					if (data == null) {
+//						log("closed by client");
+//						break;
+//					}
+//					log("received : " + data);
+//					pw.println(data);
+//				}
+//			} catch (SocketException ex) {
+//				log("suddenly closed by client");
+//			} catch (IOException ex) {
+//				log("error : " + ex);
+//			} finally {
+//				try {
+//					if (socket != null && !socket.isClosed()) {
+//						socket.close();
+//					}
+//				} catch (IOException ex) {
+//					ex.printStackTrace();
+//				}
+//			}
 		} catch (IOException e) {
 			log("error : " + e);
 		} finally {
@@ -71,6 +68,6 @@ public class EchoServer {
 	}
 
 	private static void log(String message) {
-		System.out.println("[EchoServer] " + message);
+		System.out.println("[EchoServer#"+Thread.currentThread().getId()+"] " + message);
 	}
 }
